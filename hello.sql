2afc862481afe672367f0af3290c0f38a4ade82e
yellowbud <yellow_bud@live.cn>
2021-04-29 00:28:33 +0800

    Write some HTML, CSS and JS code

---
+++ hello.sql
@@ -0,0 +1,11 @@
+create  table  example
+INSERT INTO example
+ (field1, field2, field3)
+ VALUES
+ ('test', 'N', NULL);
+ CREATE TABLE example(
+  column1 INTEGER,
+  column2 VARCHAR(50),
+  column3 DATE NOT NULL,
+  PRIMARY KEY (column1, column2)
+ );
